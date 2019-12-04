fc8be78bcae5319b3a2d2c01b7c531ad1d35c94c
AndreaHabib <54924600+AndreaHabib@users.noreply.github.com>
2019-12-03 19:32:36 -0500

    Update Templates.cpp

--- Templates.cpp
+++ Templates.cpp
@@ -6,7 +6,8 @@ template<class T>
 class Set {
 private:
 	T* set;
-	int size, counter = 0;
+	int size, counter = 0, max;
+	Set* sets;
 public:
 	Set(int num) : size(num) {
 		set = new T[num];
@@ -16,33 +17,72 @@ public:
 	void displaySet();
 	T* pointer();
 };
-
 int main() {
-	cout << "Enter size of set: ";
+	string dType;
 	int setSize;
-	cin >> setSize;
-	int test;
-	Set<int>set1(setSize);
-	for (int i = 0; i < setSize; i++) {
-		cin >> test;
-		set1.setData(test);
+	char loop = 'y';
+	while (loop == 'y') {
+		cout << "What is the data type of the set you want to create? (int, string, double, char)" << endl;
+		cin >> dType;
+		if (dType == "int") {
+			cout << "Enter size of set: ";
+			cin >> setSize;
+			int test;
+			Set<int>set1(setSize);
+			for (int i = 0; i < setSize; i++) {
+				cin >> test;
+				set1.setData(test);
+			}
+			set1.displaySet();
+			int* p1 = set1.pointer();
+			delete[] p1;
+			p1 = NULL;
+		}
+		else if (dType == "string") {
+			cout << "Enter size of set: ";
+			cin >> setSize;
+			Set<string>set2(setSize);
+			string test;
+			for (int i = 0; i < setSize; i++) {
+				cin >> test;
+				set2.setData(test);
+			}
+			set2.displaySet();
+			string* p2 = set2.pointer();
+			delete[] p2;
+			p2 = NULL;
+		}
+		else if (dType == "double") {
+			cout << "Enter size of set: ";
+			cin >> setSize;
+			Set<double>set3(setSize);
+			double test;
+			for (int i = 0; i < setSize; i++) {
+				cin >> test;
+				set3.setData(test);
+			}
+			set3.displaySet();
+			double* p3 = set3.pointer();
+			delete[] p3;
+			p3 = NULL;
+		}
+		else if (dType == "char") {
+			cout << "Enter size of set: ";
+			cin >> setSize;
+			Set<char>set4(setSize);
+			char test;
+			for (int i = 0; i < setSize; i++) {
+				cin >> test;
+				set4.setData(test);
+			}
+			set4.displaySet();
+			char* p4 = set4.pointer();
+			delete[] p4;
+			p4 = NULL;
+		}
+		cout << "Would you like to create another set?" << endl;
+		cin >> loop;
 	}
-	set1.displaySet();
-	int* p1 = set1.pointer();
-	cout << "Enter size of set: ";
-	cin >> setSize;
-	Set<string>set2(setSize);
-	string test1;
-	for (int i = 0; i < setSize; i++) {
-		cin >> test1;
-		set2.setData(test1);
-	}
-	set2.displaySet();
-	string* p2 = set2.pointer();
-	delete[] p1;
-	p1 = NULL;
-	delete[] p2;
-	p2 = NULL;
 	return 0;
 }
 template<class T>
@@ -67,24 +107,3 @@ template<class T>
 T* Set<T>::pointer() {
 	return set;
 }
-//Enter size of set : 5
-//4
-//8
-//4
-//9
-//1
-//Number of Itms in this set: 4
-//Items in this set :
-//Item 1 : 4
-//Item 2 : 8
-//Item 3 : 9
-//Item 4 : 1
-//Enter size of set : 3
-//Hey
-//Dude
-//Bruh
-//Number of Itms in this set : 3
-//Items in this set :
-//Item 1 : Hey
-//Item 2 : Dude
-//Item 3 : Bruh
